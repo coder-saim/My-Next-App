4934026fbe59bd0b8119c160ae20e20af7209e46
coder-saim <saim.cse.du.744@gmail.com>
2023-08-01 00:40:18 +0600

    dialog box added in result table

--- components/description-modal.tsx
+++ components/description-modal.tsx
@@ -1,6 +1,3 @@
-import { SchemaSection } from "./model-schema-section";
-import { ModelSummarySection } from "./model-summary-section";
-import { StatusBanner } from "./status-banner";
 import { ModelDetails } from "@/types";
 import React, { useState } from "react";
 import * as Dialog from '@radix-ui/react-dialog';
@@ -19,9 +16,9 @@ const DescriptionModal = () => {
         Click to view
       </button>
     </Dialog.Trigger>
-    <Dialog.Portal>
+    <Dialog.Portal className=" bg-black rounded-3xl">
       <Dialog.Overlay className="bg-blackA9 data-[state=open]:animate-overlayShow fixed inset-0" />
-      <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+      <Dialog.Content className="rounded-2xl data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
         <Dialog.Title className="text-mauve12 text-black text-lg font-bold m-0 text-[17px]">
           Description
         </Dialog.Title>
