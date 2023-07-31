b2fbdb630e8f36e05fc344878c1aba2265b9d3cc
coder-saim <saim.cse.du.744@gmail.com>
2023-08-01 01:06:59 +0600

    dialog done

--- components/description-modal.tsx
+++ components/description-modal.tsx
@@ -18,15 +18,11 @@ const DescriptionModal = () => {
     </Dialog.Trigger>
     <Dialog.Portal className=" bg-black rounded-3xl">
       <Dialog.Overlay className="bg-blackA9 data-[state=open]:animate-overlayShow fixed inset-0" />
-      <Dialog.Content className="rounded-2xl data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
-        <Dialog.Title className="text-black text-lg font-bold text-[17px]">
+      <Dialog.Content className="rounded-2xl divide-y-2 data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+        <Dialog.Title className="text-black pb-2 text-lg font-bold text-[17px]">
           Description
         </Dialog.Title>
-        <Dialog.Description className="text-mauve11 mt-[10px] mb-5 text-[15px] leading-normal">
-          Make changes to your profile here. Click save when you're done.
-        </Dialog.Description>
-        
-        
+
         <Dialog.Close asChild>
           <button
             className=" absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center pr-8 pt-6"
@@ -35,6 +31,13 @@ const DescriptionModal = () => {
            Close
           </button>
         </Dialog.Close>
+
+        <Dialog.Description className="text-mauve11 pt-4 mt-[10px] mb-5 text-[15px] leading-normal">
+          Make changes to your profile here. Click save when you're done.
+        </Dialog.Description>
+        
+        
+        
       </Dialog.Content>
     </Dialog.Portal>
   </Dialog.Root>
