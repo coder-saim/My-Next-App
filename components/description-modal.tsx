bbe2cafc6dc492255efa7bab1162b3894748cdc4
coder-saim <saim.cse.du.744@gmail.com>
2023-08-01 23:13:06 +0600

    dialog box z index error fixed...

--- components/description-modal.tsx
+++ components/description-modal.tsx
@@ -13,8 +13,8 @@ const DescriptionModal = () => {
         <button className="underline">Click to view</button>
       </Dialog.Trigger>
       <Dialog.Portal className=" rounded-3xl ">
-        <Dialog.Overlay className="bg-blackA9 data-[state=open]:animate-overlayShow fixed inset-0" />
-        <Dialog.Content className="rounded-2xl divide-y-2 data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+        <Dialog.Overlay className="bg-blackA9 data-[state=open]:animate-overlayShow fixed inset-0 z-50 bg-gray-700/90 data-[state=open]:animate-overlay-show" />
+        <Dialog.Content className="z-50 data-[state=open]:animate-content-show rounded-2xl divide-y-2 data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
           <Dialog.Title className="text-black pb-2 text-lg font-bold text-[17px] ">
             Description
           </Dialog.Title>
